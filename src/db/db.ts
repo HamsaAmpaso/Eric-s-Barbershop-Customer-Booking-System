c94b4c29892fa226952ac80a7a5f876652a67bdb
Hamsa Ampaso <hamsaampaso1@gmail.com>
2026-09-11 15:43:37 +0800

    deloyment config phase 2

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,11 +4,12 @@ import pg from 'pg';
 const {Pool} = pg;
 
 export const poolDB = new Pool({
-    //connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL
+    /*
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE*/
 
 });
